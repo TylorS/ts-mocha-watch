e3feaec0cd1f57cd7e8c456c5512f64af9d312d1
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-03-22 17:45:51 -0400

    feat(cli): fix errors

--- cli.js
+++ cli.js
@@ -5,12 +5,12 @@ var watch = require('./').watch
 var argv = process.argv.slice(2)
 
 var srcIndex = argv.indexOf('--src')
-var srcPatterns = argv[srcIndex + 1].split(',') || void 0
+var srcPatterns = srcIndex > -1 ? argv[srcIndex + 1].split(',') : void 0
 
 var testIndex = argv.indexOf('--test')
-var testPatterns = argv[testIndex + 1].split(',') || void 0
+var testPatterns = testIndex > -1 ? argv[testIndex + 1].split(',') : void 0
 
 var requiresIndex = argv.indexOf('--requires')
-var requires = argv[requiresIndex + 1].split(',') || void 0
+var requires = requiresIndex > -1 ? argv[requiresIndex + 1].split(',') : void 0
 
 watch(process.cwd(), srcPatterns, testPatterns, requires)
